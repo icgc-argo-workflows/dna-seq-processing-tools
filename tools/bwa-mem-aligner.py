ba5933b452e8a03a19087ad9190b710cf15f2829
lindaxiang <linda.xiang@oicr.on.ca>
2019-04-22 13:16:35 -0400

    fix for bwa

--- tools/bwa-mem-aligner.py
+++ tools/bwa-mem-aligner.py
@@ -1,32 +1,67 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import subprocess
 import argparse
+from multiprocessing import cpu_count
+import sys
 
 def main():
     """ Main program """
     parser = argparse.ArgumentParser(description='BWA alignment')
-    parser.add_argument('-i','--input-bam', dest='input_bam', type=str, help='Input bam file', required=True)
-    parser.add_argument('-r','--ref-genome', dest='ref_genome', type=str, help='Fa.gz reference genome file', required=True)
-    parser.add_argument('-o','--output', dest='output', type=str, help='Output bam file', required=True)
+    parser.add_argument('-i','--input-bam', dest='input_bam', type=str,
+                        help='Input bam file', required=True)
+    parser.add_argument('-r','--ref-genome', dest='ref_genome', type=str,
+                        help='Fa.gz reference genome file', required=True)
+    parser.add_argument('-o','--output', dest='output', type=str,
+                        help='Output bam file', required=True)
+    parser.add_argument("-n", "--cpus", type=int, default=cpu_count())
     args = parser.parse_args()
 
-    bam_header = subprocess.Popen(['samtools','view','-H',args.input_bam],stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()[0].strip()
-    rg_header = []
-    for line in bam_header.split('\n'):
-        if "@RG" in line: rg_header.append(line)
+    # retrieve the @RG from BAM header
+    try:
+        header = subprocess.check_output(['samtools', 'view', '-H', args.input_bam])
 
+    except Exception as e:
+        sys.exit('\n%s: Retrieve BAM header failed: %s' % (e, args.input_bam))
 
-    bam_to_fastq = 'samtools fastq %s' % args.input_bam
+    # get @RG
+    header_array = header.decode('utf-8').rstrip().split('\n')
+    rg_array = []
+    for line in header_array:
+        if not line.startswith("@RG"): continue
+        rg_array.append(line.rstrip().replace('\t', '\\t'))
+
+    if not len(rg_array) == 1: sys.exit('\n%s: The input bam should only contain one readgroup ID: %s' % args.input_bam)
+
+    bam2fastq = 'samtools fastq %s ' % args.input_bam
 
     #Command with header
-    #alignment = 'bwa mem -R %s %s -' % (''.join(rg_header), args.ref_genome)
+    alignment = ' bwa mem -t %s -p -R "%s" %s - > %s' % (str(args.cpus), rg_array[0], args.ref_genome, args.output)
 
-    #Command without header
-    alignment = 'bwa mem %s -' % (args.ref_genome)
+    cmd = '|'.join([bam2fastq, alignment])
 
-    subprocess.call(' '.join([bam_to_fastq,'|',alignment,'>',args.output]),shell=True)
+    print('command: %s' % cmd)
+    stdout, stderr, p, success = '', '', None, True
+    try:
+        p = subprocess.Popen([cmd],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=True)
+        stdout, stderr = p.communicate()
+    except Exception as e:
+        print('Execution failed: %s' % e, file=sys.stderr)
+        success = False
+
+    if p and p.returncode != 0:
+        print('Execution failed, none zero code returned.', file=sys.stderr)
+        success = False
+
+    print(stdout.decode("utf-8"))
+    print(stderr.decode("utf-8"), file=sys.stderr)
+
+    if not success:
+        sys.exit(p.returncode if p.returncode else 1)
 
 if __name__ == "__main__":
     main()
