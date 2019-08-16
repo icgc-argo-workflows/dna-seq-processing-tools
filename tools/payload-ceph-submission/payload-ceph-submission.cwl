110b3fd0fcd977b5537aec680537748f4d086c9c
lindaxiang <linda.xiang@oicr.on.ca>
2019-08-16 14:37:14 -0400

    initial commit for payload-ceph-submission

---
+++ tools/payload-ceph-submission/payload-ceph-submission.cwl
@@ -0,0 +1,45 @@
+class: CommandLineTool
+cwlVersion: v1.1
+id: payload-generation
+requirements:
+- class: InlineJavascriptRequirement
+- class: ShellCommandRequirement
+- class: DockerRequirement
+  dockerPull: 'quay.io/icgc-argo/payload-ceph-submission:payload-ceph-submission.initial'
+- class: EnvVarRequirement
+  envDef:
+    - envName: "AWS_CONFIG_FILE"
+      envValue: $(inputs.aws_config_file.path)
+    - envName: "AWS_SHARED_CREDENTIALS_FILE"
+      envValue: $(inputs.aws_shared_credentials_file.path)
+
+baseCommand: [ 'payload-ceph-submission.py' ]
+
+inputs:
+  aws_config_file:
+    type: File
+  aws_shared_credentials_file:
+    type: File
+  payload:
+    type: File
+    inputBinding:
+      prefix: -p
+  metadata:
+    type: File
+    inputBinding:
+      prefix: -m
+  endpoint_url:
+    type: string
+    inputBinding:
+      prefix: -s
+  bucket_name:
+    type: string
+    inputBinding:
+      prefix: -b
+
+outputs:
+  payload:
+    type: File
+    outputBinding:
+      glob: '*.json'
+
---
+++ tools/payload-ceph-submission/payload-ceph-submission.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+import subprocess
+from argparse import ArgumentParser
+import uuid
+
+"""
+Major steps:
+- convert input Seq to unaligned BAM for each read group
+"""
+
+
+def run_cmd(cmd):
+    stdout, stderr, p, success = '', '', None, True
+    try:
+        p = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=True)
+        p.communicate()
+    except Exception as e:
+        print('Execution failed: %s' % e)
+        success = False
+
+    if p and p.returncode != 0:
+        print('Execution failed, none zero code returned. %s' % p.returncode)
+        success = False
+
+    if not success:
+        sys.exit(p.returncode if p.returncode else 1)
+
+    return
+
+
+def get_uuid5(pid, fid):
+    uuid5 = str(uuid.uuid5(uuid.UUID("6ba7b810-9dad-11d1-80b4-00c04fd430c8"), "%s/%s" % (pid, fid)))
+    return uuid5
+
+def main(args):
+
+    with open(args.metadata, 'r') as f:
+        metadata = json.load(f)
+
+    with open(args.payload, 'r') as f:
+        payload = json.load(f)
+
+    # generate bundle_id
+    payload['id'] = str(uuid.uuid4())
+
+    # generate info field
+    payload['info'] = {
+        "library_strategy": metadata.get("library_strategy", None),
+        "program_id": metadata.get("program", None),
+        "submitter_donor_id": metadata.get("submitter_donor_id", None),
+        "submitter_sample_id": metadata.get("submitter_sample_id", None),
+        "specimen_type": 'normal' if 'normal' in metadata.get("specimen_type", '').lower() else 'tumour'
+    }
+
+    # generate object_id
+    for key, val in payload['files'].items():
+        val['object_id'] = get_uuid5(payload['info']['program_id'], val['name'])
+
+    payload_fname = ".".join([payload['id'], 'json'])
+    with open(payload_fname, 'w') as f:
+        f.write(json.dumps(payload))
+
+    # payload bucket basepath
+    payload_bucket_basepath = os.path.join(args.bucket_name, 'PCAWG2',
+                                        payload['info']['library_strategy'],
+                                        payload['info']['program_id'],
+                                        payload['info']['submitter_donor_id'],
+                                        payload['info']['submitter_sample_id'] + '.' + payload['info']['specimen_type'],
+                                        payload['type'])
+
+    if payload['type'] in ['sequencing_experiment', 'dna_alignment_qc']:
+        payload_object = os.path.join(payload_bucket_basepath, payload_fname)
+    elif payload['type'] in ['lane_seq_submission', 'lane_seq_qc']:
+        payload_object = os.path.join(payload_bucket_basepath, payload['inputs']['read_group_submitter_id'], payload_fname)
+
+    elif payload['type'] in ['dna_alignment']:
+        alignment_type = "bam" if payload['files']['aligned_seq']['name'].endswith('bam') else "cram"
+        payload_object = os.path.join(payload_bucket_basepath, alignment_type, payload_fname)
+
+    else:
+        sys.exit('Unknown payload type!')
+
+    cmd = "aws --endpoint-url %s s3 cp %s s3://%s" % (args.endpoint_url, payload_fname, payload_object)
+    run_cmd(cmd)
+
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--s3-endpoint-url", dest="endpoint_url")
+    parser.add_argument("-b", "--bucket-name", dest="bucket_name")
+    parser.add_argument("-m", "--metadata", dest="metadata", help="Metadata file contains the information user submit")
+    parser.add_argument("-p", "--payload", dest="payload", help="Payload file")
+    args = parser.parse_args()
+
+    main(args)
