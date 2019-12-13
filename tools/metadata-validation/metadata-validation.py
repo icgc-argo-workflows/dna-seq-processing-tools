581da5d84f35173d06ea32a3e224da3c3aff85be
lindaxiang <linda.xiang@oicr.on.ca>
2019-12-13 08:54:34 -0500

    re-define the input/output of the tool

--- tools/metadata-validation/metadata-validation.py
+++ tools/metadata-validation/metadata-validation.py
@@ -5,7 +5,7 @@ import csv
 from argparse import ArgumentParser
 import json
 import datetime
-import copy
+
 
 def set_default(obj):
     if isinstance(obj, datetime.datetime):
@@ -15,33 +15,6 @@ def set_default(obj):
     if isinstance(obj, set) and len(obj) == 1:
         return obj.pop()
     raise TypeError
-
-def reshape_metadata(input_metadata):
-    output_metadata = {}
-    output_files = {}
-    for key, value in input_metadata.items():
-        if not key == 'read_groups':
-            output_metadata[key] = value
-            continue
-        # process readGroups
-        for rg in input_metadata['read_groups']:
-            output_rg = {}
-            for k, v in rg.items():
-                if not k == 'files':
-                    output_rg[k] = v
-                    continue
-                for fn in rg['files']:
-                    if not fn['name'] in output_files:
-                        output_files[fn['name']] = {}
-                        output_files[fn['name']]['read_groups'] = []
-                    for fk, fv in fn.items():
-                        if not fk in output_files[fn['name']]: output_files[fn['name']][fk] = set()
-                        output_files[fn['name']][fk].add(fv)
-            output_files[fn['name']]['read_groups'].append(output_rg)
-    output_metadata['files'] = []
-    for key, value in output_files.items():
-        output_metadata['files'].append(value)
-    return output_metadata
 
 
 def build_entity_map(entity_tsv, pointer_id):
@@ -63,16 +36,6 @@ def build_entity_map(entity_tsv, pointer_id):
             entity_dict[l[pointer_id]].append(sub_entity_dict)
     return entity_dict
 
-def get_input_format(file_tsv):
-    with open(file_tsv, 'r') as f:
-        reader = csv.DictReader(f, delimiter='\t')
-        input_format = set()
-        for l in reader:
-            input_format.add(l.get('format').upper())
-
-    if not len(input_format) == 1: sys.exit('\nError: The input files should have the same format.')
-
-    return input_format.pop()
 
 def generate_metadata(args):
 
@@ -113,58 +76,23 @@ def generate_metadata(args):
 
 def run_validation(args):
 
-    # generate the exp_json with experiment, read_group, file info
-    if args.meta_format == 'tsv':
-        input_seq_format = get_input_format(args.file_tsv)
-        exp_json = generate_metadata(args)
-    else:
-        exp_json = args.exp_json
+    # generate the metadata with experiment, read_group, file tsv info
+    metadata = generate_metadata(args)
 
-    # generate metadata from exp_json by reshaping the exp_json in condition of the input_format
-    # cross check the read_group id for BAM
-    if input_seq_format == "BAM":
-        metadata = reshape_metadata(exp_json)
-        metadata["input_seq_format"] = "BAM"
-
-    elif input_seq_format == "FASTQ":
-        metadata = copy.deepcopy(exp_json)
-        metadata["input_seq_format"] = "FASTQ"
-
-    else:
-        sys.exit('\nError: The input files should have format in BAM or FASTQ.')
+    # additional checks
 
 
-    with open(args.metadata_json, 'w') as f:
+
+    # write the metadata.json as output
+    with open('metadata.json', 'w') as f:
         f.write(json.dumps(metadata, default=set_default, indent=2))
-
-
-    # remove files from the exp_json
-    # validate the json with the given schema and the data get from the server
-    for rg in exp_json['read_groups']:
-        rg.pop('files')
-
-    # write the exp_rg.json file for later submission
-    with open(args.exp_rg_json, 'w') as f:
-        f.write(json.dumps(exp_json, default=set_default, indent=2))
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("-m", "--meta", dest="meta_format", type=str, help="metadata file format",
-                        choices=["tsv", "json"],
-                        default="tsv")
-    parser.add_argument("-j", "--exp_json", dest="exp_json", help="json format file containing readgroup and input BAM/FASTQ file information")
     parser.add_argument("-e", "--exp_tsv", dest="exp_tsv", help="tsv format file containing experiment information")
     parser.add_argument("-r", "--rg_tsv", dest="rg_tsv", help="tsv format file containing readgroup information")
     parser.add_argument("-f", "--file_tsv", dest="file_tsv", help="tsv format file containing BAM/FASTQ input file information")
-    parser.add_argument("-o", "--exp_rg_json", dest="exp_rg_json", help="json file containing experiment and read_group information", required=True)
-    parser.add_argument("-p", "--metadata_json", dest="metadata_json",
-                        help="json file containing experiment, read_group and file information for sequence preprocessing", required=True)
     args = parser.parse_args()
-
-    if (args.meta_format == 'json' and not args.exp_json):
-        parser.error('The json input argument requires the --exp-json')
-    if (args.meta_format == 'tsv' and (not args.exp_tsv or not args.rg_tsv or not args.file_tsv)):
-        parser.error('The tsv input argument requires both --exp-tsv, --rg-tsv and --file-tsv')
 
     run_validation(args)
